44e95a070bda10e0abd5a3c72370684210b08660
Carlos <itscarlosisaac@gmail.com>
2021-04-28 11:33:59 -0400

    Updated app to use login local storage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import { AuthProvider } from './context/Auth/AuthContext';
 import { AuthReducer } from './context/Auth/AuthReducer';
+import { useLocalStorage } from './hooks/useLocalStorage';
 import { AppRouter } from './routes/AppRouter'
 
+
 function App() {
-  const [user, dispatch] = useReducer(AuthReducer, {logged: false})
+  const [, getLocalData ] = useLocalStorage()
+  const [ user, dispatch] = useReducer(AuthReducer, {}, () => getLocalData("ReactPlaybookLogin") || { logged: false })
+
   return (
     <AuthProvider value={{user, dispatch}}>
       <AppRouter />
