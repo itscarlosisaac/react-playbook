f7cec51b195811441d89fb9cc621ba8bbab5cca7
Carlos <itscarlosisaac@gmail.com>
2021-04-29 19:10:27 -0400

    Added canvas type switch.

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Card } from '../components/Card/Card'
 import { Box } from '../components/Card/Box'
 import { Documentation } from '../components/Documentation';
+import { ViewContext } from '../context/View/ViewContext';
 
 export const Canvas = () => {
+
+  const { view } = useContext(ViewContext);
+
+  const checkViewType = (Component) => {
+    switch (view.view) {
+      case "component":
+        return (<Component />)
+      case "documentation":
+        return (<Documentation/>)
+      case "split":
+        return (<><Component /> <Documentation/></>)
+      default:
+        return (<Component />)
+    }
+  }
+
   return (
     <div>
-      <h1>Canvas here</h1>
+      <h1>Canvas Containeer</h1>
       <Switch>
-        <Route path="/card" components={{card: Card, doc: Documentation}} />
-        <Route path="/box" component={Box} />
+        <Route path="/card" render={() => checkViewType(Card) } />
+        <Route path="/box" render={() => checkViewType(Box) } />
       </Switch>
     </div>
   )
